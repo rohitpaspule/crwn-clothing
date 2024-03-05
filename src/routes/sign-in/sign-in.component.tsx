3952f99233f4317e9ee040d605c18f45c9b98ca1
rohitpaspule1 <rohitpaspule@gmail.com>
2024-03-05 19:22:34 +0530

    firebase update

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,6 +1,17 @@
+import { signInWithGooglePopup , createUserDocumentFromAuth} from "../../utils/firebase.utils";
+
 const SignIn  = () =>{
+    const logGoogleUser = async() =>{
+       const response = await signInWithGooglePopup(); 
+       const {user} =  response
+       const userDocRef =  await createUserDocumentFromAuth(user);
+    }
     return(<>
-    Sign in
+   <h1> Sign in Page</h1>
+   <button onClick={()=>{
+    logGoogleUser();
+   }}>Signin</button>
+
     </>)
 }
 export default SignIn
